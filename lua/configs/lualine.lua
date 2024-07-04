67adbcd539e12a94b1033d813245e61c388207ec
Sushant <sushant.dhiman9812@gmail.com>
2024-07-04 12:10:54 +0000

    Added lualine

---
+++ lua/configs/lualine.lua
@@ -0,0 +1,20 @@
+local options = {
+    icons_enabled = true,
+    theme = 'gruvbox_dark',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+}
+
+return options
