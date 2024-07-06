5d6fb6ac12096e655c57e4cc41a5f3bdb6101b19
Sushant <sushant.dhiman9812@gmail.com>
2024-07-06 08:46:18 +0530

    added - rust support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,6 +5,7 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "gopls", "tsserver", "rust_analyzer" }
+local util = require "lspconfig/util"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -14,6 +15,20 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust_analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      }
+    }
+  }
+})
 
 -- typescript
 -- lspconfig.tsserver.setup {
